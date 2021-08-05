198b630ef6a4147eb93d04a12558d25242083d37
Erick Arias <erickarias1999@hotmail.com>
2021-08-05 11:17:04 -0500

    PART IV, Chapter 22, Filling Arrays, Explanation Part

---
+++ PART-IV-ManagingDataWithYourCPrograms/22.SearchingArrays/FillingArrays.c
@@ -0,0 +1,18 @@
+/* You won’t become an ace programmer if you aren’t exposed a bit to searching and sorting values. */
+
+
+// FILLING ARRAYS
+
+/*
+Your programs can and will fill arrays using a combination of all three of these methods:
+
+    • Assignment
+
+    • User data entry
+
+    • Disk files
+
+NOTE:
+Concentrate on what you do with arrays after the arrays get filled with values.
+One of the most important things to do is find values that you put in the arrays.
+*/
