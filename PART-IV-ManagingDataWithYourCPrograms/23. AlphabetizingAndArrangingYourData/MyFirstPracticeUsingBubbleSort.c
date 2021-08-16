319832127057165e424a966b96e24a0200f599bf
Erick Arias <erickarias1999@hotmail.com>
2021-08-16 17:13:48 -0500

    PART IV, Chapter 23, My First Practice Using Bubble Sort

--- PART-IV-ManagingDataWithYourCPrograms/23. AlphabetizingAndArrangingYourData/MyFirstPracticeUsingBubbleSort.c
+++ PART-IV-ManagingDataWithYourCPrograms/23. AlphabetizingAndArrangingYourData/MyFirstPracticeUsingBubbleSort.c
@@ -8,7 +8,8 @@ main()
 {
     // SET UP VARIABLES
     int nums[5] = {84, 12, 105, 47, 368};
-    int aux, ctr, inner, outer;
+    int aux, ctr, inner, outer, didSwap;
+    aux = 0;
 
     printf("This is the list of 5 numbers:\n");
     for (ctr = 0; ctr < 5; ctr++)
@@ -18,16 +19,30 @@ main()
 
     // SORT NUMBERS
 
-    for (outer = 0; outer < 9; outer++)
+    for (outer = 0; outer < 4; outer++)
     {
-        for (inner = outer; inner < 10; inner--)
+        didSwap = 0; //Becomes 1 (true) if list is not yet ordered
+        for (inner = outer; inner < 5; inner++)
         {
-
+            if (nums[inner] < nums[outer])
+            {
+                aux = nums[inner];
+                nums[inner] = nums[outer];
+                nums[outer] = aux;
+                didSwap = 1;
+            }
+        }
+        if (didSwap == 0)
+        {
+            break;
         }
     }
 
     printf("And here is the sort list of numbers:\n");
-
+    for (ctr = 0; ctr < 5; ctr++)
+    {
+        printf("%d\n", nums[ctr]);
+    }
 
     return 0;
 }
