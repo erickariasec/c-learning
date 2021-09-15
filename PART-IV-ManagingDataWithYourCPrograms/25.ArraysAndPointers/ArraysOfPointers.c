8ab34976b11d34380478d82b65f33a948fd15993
Erick Arias <erickarias1999@hotmail.com>
2021-09-15 08:20:09 -0500

    PART IV, Chapter 25, Arrays of Pointers, Theorical Part, Important Explanation

---
+++ PART-IV-ManagingDataWithYourCPrograms/25.ArraysAndPointers/ArraysOfPointers.c
@@ -0,0 +1,12 @@
+// ARRAYS OF POINTERS
+
+/*
+If you want to use a bunch of pointers, create an array of them. An array of pointers is just as easy to define as an array of any other
+kind of data, except that you must include the * operator after the data type name.
+
+The following statements reserve an array of 25 integer pointers and an array of 25 character pointers:
+
+        int * ipara[25];   // 25 pointers to integers
+        char * cpara[25];  // 25 pointers to characters
+
+*/
